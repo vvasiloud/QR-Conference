8c5e2ffdf7ce72ecaf27cc68c8b712479392574a
vvasiloud <vvasiloudis@gmail.com>
2016-11-14 19:37:12 +0200

    Last activity to checkin function added

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\AttendeeActivity;
+use App\Models\Attendee;
+use App\Models\Venue;
+use App\Models\Auditorium;
 
 class VenueController extends Controller
 {
@@ -15,8 +19,32 @@ class VenueController extends Controller
     }
 	
 	public function checkin($venue_id, $attendee_id){
+		$action = '';
+
+		$lastActivity = AttendeeActivity::where('attendee_id', $attendee_id)->where('venue_id', $venue_id)->orderBy('updated_at', 'desc')->first();
+		if($lastActivity){
+			$action = ($lastActivity->action == 'checkin') ? 'checkout' : 'checkin';
+		}
+		
+        $attendeeActivity = new AttendeeActivity;
+		$attendeeActivity->attendee_id = $attendee_id;
+        $attendeeActivity->venue_id = $venue_id;
+        $attendeeActivity->auditorium_id = 1;
+		$attendeeActivity->action = ($action != '') ? $action : 'checkin';
+        $attendeeActivity->save();
+		
+		$attendeeName = Attendee::find($attendee_id)->name;
+		//$auditoriumName = Auditorium::find($auditorium_id)->name;
+		$venueName = Venue::find($venue_id)->name;
+		
 		$data['venue_id'] = $venue_id;
-		$data['attendee_id'] = $attendee_id;
+		$data['attendee_id'] = $attendee_id;		
+		$data['attendee_name'] = $attendeeName;
+		$data['attendee_action'] = $action;
+		//$data['auditorium_name'] = $auditoriumName;
+		$data['venue_name'] = $venueName;
+
+		
 		return view('checkin', $data);
 	}
 }
