303b8b27964522605a19756a23ff2112c1e29b08
vvasiloud <vvasiloudis@gmail.com>
2016-11-19 11:34:06 +0200

    Fix: Rectrict controller update action when user is not loggedin

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -20,64 +20,68 @@ class VenueController extends Controller
     }
 	
 	public function checkin($venue, $attendee){
-		$action = '';
+		$data =[];
+		if(\Auth::check()){
+			$action = '';
 
-		$attendee_id = $attendee->id;
-		$attendeeName = $attendee->name;
-		$venue_id = $venue->id;
-		$venueName = $venue->name;
-		//$auditorium_id = $auditorium->id;
-		//$auditoriumName = $auditorium->name;
+			$attendee_id = $attendee->id;
+			$attendeeName = $attendee->name;
+			$venue_id = $venue->id;
+			$venueName = $venue->name;
+			//$auditorium_id = $auditorium->id;
+			//$auditoriumName = $auditorium->name;
 
-		$lastActivity = AttendeeActivity::where('attendee_id', $attendee_id)->where('venue_id', $venue_id)->orderBy('updated_at', 'desc')->first();
-		if($lastActivity){
-			$action = ($lastActivity->action == 'checkin') ? 'checkout' : 'checkin';
+			$lastActivity = AttendeeActivity::where('attendee_id', $attendee_id)->where('venue_id', $venue_id)->orderBy('updated_at', 'desc')->first();
+			if($lastActivity){
+				$action = ($lastActivity->action == 'checkin') ? 'checkout' : 'checkin';
+			}
+			
+			$attendeeActivity = new AttendeeActivity;
+			$attendeeActivity->attendee_id = $attendee_id;
+			$attendeeActivity->venue_id = $venue_id;
+			$attendeeActivity->auditorium_id = 1;
+			$attendeeActivity->action = ($action != '') ? $action : 'checkin';
+			$attendeeActivity->save();
+			
+			$data['attendee_id'] = $attendee_id;		
+			$data['attendee_name'] = $attendeeName;
+			$data['attendee_action'] = $action;
+			$data['venue_id'] = $venue_id;
+			$data['venue_name'] = $venueName;
+			//$data['auditorium_name'] = $auditoriumName;
 		}
-		
-        $attendeeActivity = new AttendeeActivity;
-		$attendeeActivity->attendee_id = $attendee_id;
-        $attendeeActivity->venue_id = $venue_id;
-        $attendeeActivity->auditorium_id = 1;
-		$attendeeActivity->action = ($action != '') ? $action : 'checkin';
-        $attendeeActivity->save();
-		
-		$data['attendee_id'] = $attendee_id;		
-		$data['attendee_name'] = $attendeeName;
-		$data['attendee_action'] = $action;
-		$data['venue_id'] = $venue_id;
-		$data['venue_name'] = $venueName;
-		//$data['auditorium_name'] = $auditoriumName;
-		
 		return view('checkin', $data);
 	}
 	
 	public function massCheckout($venue){
-		$venue_id = $venue->id;
 		$data =[];
-		$attendeeArray = [];
-		
-		$lastAttendeeActivities = AttendeeActivity::select(DB::raw('MAX(updated_at) AS updated_at'), 'attendee_id','action', DB::raw('MAX(id) AS id'))
-								->where('venue_id', $venue_id)
-								->groupBy('attendee_id','action')
-								->orderBy('attendee_id','asc')->orderBy('updated_at','asc')
-								->get();
-		$keyed = $lastAttendeeActivities->keyBy('attendee_id');
+		if(\Auth::check()){
+			$venue_id = $venue->id;
+			$attendeeArray = [];
+			
+			$lastAttendeeActivities = AttendeeActivity::select(DB::raw('MAX(updated_at) AS updated_at'), 'attendee_id','action', DB::raw('MAX(id) AS id'))
+									->where('venue_id', $venue_id)
+									->groupBy('attendee_id','action')
+									->orderBy('attendee_id','asc')->orderBy('updated_at','asc')
+									->get();
+			$keyed = $lastAttendeeActivities->keyBy('attendee_id');
 
-		foreach ($keyed as $attendee):
-			if($attendee->action == 'checkin'):
-			    $attendeeActivity = new AttendeeActivity;
-				$attendeeActivity->attendee_id = $attendee->attendee_id;
-				$attendeeActivity->venue_id = $venue_id;
-				$attendeeActivity->auditorium_id = 1;
-				$attendeeActivity->action = 'checkout';
-				$attendeeActivity->save();
-				$attendeeArray[] = $attendeeActivity;
-			endif;
-		endforeach;
+			foreach ($keyed as $attendee):
+				if($attendee->action == 'checkin'):
+					$attendeeActivity = new AttendeeActivity;
+					$attendeeActivity->attendee_id = $attendee->attendee_id;
+					$attendeeActivity->venue_id = $venue_id;
+					$attendeeActivity->auditorium_id = 1;
+					$attendeeActivity->action = 'checkout';
+					$attendeeActivity->save();
+					$attendeeArray[] = $attendeeActivity;
+				endif;
+			endforeach;
 
-		if($attendeeArray){
-			$data['checkoutAttendees'] = $attendeeArray;
+			if($attendeeArray){
+				$data['checkoutAttendees'] = $attendeeArray;
+			}
+			return view('masscheckout', $data);
 		}
-		return view('masscheckout', $data);
 	}
 }
